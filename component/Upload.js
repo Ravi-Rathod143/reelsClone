1bc5455be017884a2ed827bc34c24d6206acd53f
Ravikant Rathod <ravikantrathod143@gmail.com>
2022-11-21 11:17:34 +0530

    upload video button and loader completed

--- component/Upload.js
+++ component/Upload.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import {Button,IconButton} from '@mui/material';
+import { Button, IconButton, LinearProgress } from '@mui/material';
 // import IconButton from '@mui/material/IconButton';
 // import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import MovieIcon from '@mui/icons-material/Movie';
+
+
+
+
+ 
+
 function Upload() {
     return (
         <div className='upload-button'>
-            <Button size='small' color="secondary" variant="outlined" component="label" fullWidth 
-            
-            startIcon={<MovieIcon/>}
+            <Button size='mediun' color="secondary" variant="outlined" component="label" fullWidth
+
+                startIcon={<MovieIcon />}
 
             >
                 Upload Videos
                 <input hidden accept="*" multiple type="file" />
             </Button>
+            <LinearProgress color='secondary' variant='determinate' value={"80"} sx={{mt:"0.3rem"}} />
         </div>
     )
 }
